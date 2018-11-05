1d0ea0349ecda98a01e0e35756d5e2f0f9af045c
Manuel Weichselbaumer <mincequi@web.de>
2018-11-05 19:56:56 +0100

    Extend Peripheral test

--- ble/tests/peripheraltest.cpp
+++ ble/tests/peripheraltest.cpp
@@ -1,12 +1,18 @@
 #include <QCoreApplication>
 
 #include <ble/Peripheral.h>
+#include <ble/PeripheralAdapter.h>
 
 int main(int argc, char** argv)
 {
     QCoreApplication app(argc, argv);
 
-    new ble::Peripheral();
+    auto peripheral = new ble::Peripheral();
+    new ble::PeripheralAdapter(peripheral, {
+        { common::FilterType::Peak, 2335.0, -9.7, 15.7 },
+        { common::FilterType::LowPass, 12222.0, 3.4, 0.5 },
+        { common::FilterType::HighPass, 47.0, -8.3, 0.8 }
+    });
 
     return app.exec();
 }
