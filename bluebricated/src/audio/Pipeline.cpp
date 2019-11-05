fa8493cceac79b05243eb68de787470c92c39774
Manuel Weichselbaumer <mincequi@web.de>
2019-11-05 20:04:55 +0100

    Extend queue size

--- bluebricated/src/audio/Pipeline.cpp
+++ bluebricated/src/audio/Pipeline.cpp
@@ -37,6 +37,8 @@ Pipeline::Pipeline(Type type)
     // Common elements
     m_crAppSource = Gst::ElementFactory::create_element("cr_appsrc");
     auto queue = Gst::Queue::create();
+    queue->property_max_size_time().set_value(2000000000);
+    queue->property_max_size_buffers().set_value(2000);
     auto depay = Gst::ElementFactory::create_element("rtpsbcdepay");
     auto parse = Gst::ElementFactory::create_element("sbcparse");
     auto decoder = Gst::ElementFactory::create_element("sbcdec");
@@ -48,7 +50,7 @@ Pipeline::Pipeline(Type type)
     m_alsaSink->set_property("sync", false);    // Avoid resync since it causes ugly glitches
 
     m_pipeline->add(m_crAppSource)->add(queue)->add(depay)->add(parse)->add(decoder)->add(bluetoothConverter)->add(m_peq)->add(m_loudness)->add(alsaConverter)->add(m_alsaSink);
-    m_crAppSource->link(queue)->link(depay)->link(parse)->link(decoder)->link(bluetoothConverter)->link(m_peq)->link(m_loudness)->link(alsaConverter)->link(m_alsaSink);
+    m_crAppSource->link(queue)->link(depay)->link(parse)->link(decoder)->link(bluetoothConverter)->/*link(m_peq)->link(m_loudness)->link(alsaConverter)->*/link(m_alsaSink);
 }
 
 Pipeline::~Pipeline()
